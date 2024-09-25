9502734c3e2ae68b15853dcb28b13f887e4f5ab2
Panagopulos <tpanagopulos2@gmail.com>
2024-09-25 17:14:39 +0200

    Create a function for spinning logic.

--- project.js
+++ project.js
@@ -8,9 +8,22 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
 
+const SYMBOLS_COUNT = {
+    A : 2,
+    B : 4,
+    C : 6,
+    D : 8
+}
 
-
+const SYMBOL_VALUES = {
+    A : 5,
+    B : 4,
+    C : 3,
+    D : 2
+}
 
 
 const deposit = () => {
@@ -54,6 +67,38 @@ const getBet = (balance, lines) => {
     }
 }
 
+//Function to spin the slot machine( fill it with randomized symbols )
+const spin = () => {
+    const symbols = [];  //Main array to contain object symbols and their count
+    //Iterates through SYMBOLS_COUNT object creating pairs of symbol and count
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        //For Each symbol iterates as many times at it has counts (you can find it in the object on top)
+        for (let i = 0; i < count; i++) { 
+            symbols.push(symbol);       //pushes the symbol inside the arrray
+        }
+    }
+    //Array to contain nested arrays which will contain 3 symbols each
+    const reels = [];
+    //First we iterate through all(3) Columns and push a nested array/row inside 
+    for (let i = 0; i < COLS; i++) {
+        reels.push([]);
+        //Copy the symbols array with spread operator to have unique group of symbols for each reel
+        const reelSymbols = [...symbols];
+        //For each row(3) we add a random symbol and then push it into the neste reel 
+        for( let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+
+    return reels;
+};
+
+
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const spinIt = spin();
